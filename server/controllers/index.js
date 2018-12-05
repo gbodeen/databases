e41738ff9024dc7690bcc555c732304ab16dd599
gbodeen <gabebodeen@gmail.com>
2018-12-05 17:18:23 -0600

    Create SQL schemata for tables.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,37 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) { 
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) { 
+      
+    }, // a function which handles posting a message to the database
+    options: function (req, res) {
+      sendResponse(res, null);
+    }
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) { },
+    post: function (req, res) { },
+    options: function (req, res) {
+      sendResponse(res, null);
+    }
   }
 };
 
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
+
+sendResponse = function (response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,36 @@
 CREATE DATABASE chat;
-
 USE chat;
-
+CREATE TABLE users (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name varchar
+(255) NOT NULL
+)ENGINE=INNODB;
 CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  message varchar
+(255) DEFAULT ''
+)ENGINE=INNODB;
+CREATE TABLE rooms (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  room varchar
+(255) DEFAULT 'Lobby'
+)ENGINE=INNODB;
+CREATE TABLE chats  (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userId int NOT NULL,
+  roomId int NOT NULL,
+  messageID int NOT NULL,
+  createdAt date NOT NULL,
+  FOREIGN KEY
+(userId) REFERENCES users
+(id) ,
+  FOREIGN KEY
+(messageId) REFERENCES messages
+(id),
+  FOREIGN KEY
+(roomId) REFERENCES rooms
+(id)
+)ENGINE=INNODB;
 
 /* Create other tables and define schemas for them here! */
 
