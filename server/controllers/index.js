603c58c9ba5a325ab37007e18c355652eb659d63
gbodeen <gabebodeen@gmail.com>
2018-12-05 19:31:45 -0600

    Fix some of tests and partial implementation of GET and POST requests

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,25 @@
 var models = require('../models');
+var dbConnection = require('../db/index.js').dbConnection;
+console.log('************', dbConnection);
 
 module.exports = {
   messages: {
-    get: function (req, res) { 
-
+    get: function (req, res) {
+      //console.log('#######1', req);
+      //console.log('#######2', res);
+      let queryString = 'SELECT * FROM chats';
+      dbConnection.connect();
+      dbConnection.query(queryString, [], function (err, results) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('@@@@@@@@@@@', results);
+        }
+      });
+      dbConnection.end();
     }, // a function which handles a get request for all messages
-    post: function (req, res) { 
-      
+    post: function (req, res) {
+
     }, // a function which handles posting a message to the database
     options: function (req, res) {
       sendResponse(res, null);
--- server/schema.sql
+++ server/schema.sql
@@ -1,36 +1,32 @@
 CREATE DATABASE chat;
+
 USE chat;
+
 CREATE TABLE users (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name varchar
-(255) NOT NULL
-)ENGINE=INNODB;
+  id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+) ENGINE=INNODB;
+
 CREATE TABLE messages (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  message varchar
-(255) DEFAULT ''
-)ENGINE=INNODB;
+  id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message VARCHAR(255) DEFAULT ''
+) ENGINE=INNODB;
+
 CREATE TABLE rooms (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  room varchar
-(255) DEFAULT 'Lobby'
-)ENGINE=INNODB;
-CREATE TABLE chats  (
-  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  userId int NOT NULL,
-  roomId int NOT NULL,
-  messageID int NOT NULL,
-  createdAt date NOT NULL,
-  FOREIGN KEY
-(userId) REFERENCES users
-(id) ,
-  FOREIGN KEY
-(messageId) REFERENCES messages
-(id),
-  FOREIGN KEY
-(roomId) REFERENCES rooms
-(id)
-)ENGINE=INNODB;
+  id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  room VARCHAR(255) DEFAULT 'Lobby'
+) ENGINE=INNODB;
+
+CREATE TABLE chats (
+  id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  userId INT(3) NOT NULL,
+  roomId INT(3) NOT NULL,
+  messageID INT(3) NOT NULL,
+  createdAt DATE NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id),
+  FOREIGN KEY (messageId) REFERENCES messages(id),
+  FOREIGN KEY (roomId) REFERENCES rooms(id)
+) ENGINE=INNODB;
 
 /* Create other tables and define schemas for them here! */
 
